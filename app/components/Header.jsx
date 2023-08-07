8ec68c8427ece30ea37bfb592c564241674bfdf5
yagoramires <yago.ramiresx@gmail.com>
2023-08-06 21:35:33 -0300

    feat: home styles

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,22 +1,59 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const path = usePathname();
+
+  console.log(path);
+
   return (
     <header className='flex justify-between items-center gap-4 p-4'>
-      <Link href='/' className=''>
+      <Link
+        href='/'
+        className={`font-mediumedium ${
+          path === '/' &&
+          'bg-zinc-700 px-[12px] py-[2px] rounded-full text-white'
+        }`}
+      >
         Home
       </Link>
-      <Link href='/characters'>Characters</Link>
+      <Link
+        href='/characters'
+        className={`font-medium ${
+          path === '/characters' &&
+          'bg-zinc-700 px-[12px] py-[2px] rounded-full text-white'
+        }`}
+      >
+        Characters
+      </Link>
       <Image
         src='/assets/logo.png'
         width={200}
         height={100}
         alt='logo'
         priority
+        style={{ filter: 'grayscale(100%) invert(200%)' }}
       />
-      <Link href='/episodes'>Episodes</Link>
-      <Link href='/locations'>Locations</Link>
+      <Link
+        href='/episodes'
+        className={`font-medium ${
+          path === '/episodes' &&
+          'bg-zinc-700 px-[12px] py-[2px] rounded-full text-white'
+        }`}
+      >
+        Episodes
+      </Link>
+      <Link
+        href='/locations'
+        className={`font-medium ${
+          path === '/locations' &&
+          'bg-zinc-700 px-[12px] py-[2px] rounded-full text-white'
+        }`}
+      >
+        Locations
+      </Link>
     </header>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,43 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className='p-4 flex flex-col lg:flex-row items-center justify-center gap-8 relative'>
+      <div className=' w-full lg:w-1/2'>
+        <h1 className='font-extrabold text-zinc-600 text-4xl mb-2'>
+          Rick & <br /> Morty API
+        </h1>
+
+        <h3 className='mb-4 font-medium pl-2 border-l-2 border-zinc-700 text-zinc-700'>
+          Find your favorites rick and morty
+          <br /> characters
+        </h3>
+
+        <form className='w-full'>
+          <button type='submit'></button>
+          <input
+            type='text'
+            className='px-4 py-2 w-full lg:w-[400px] rounded-full'
+            placeholder='Search...'
+          />
+        </form>
+      </div>
+      <Image
+        src='/assets/home-img.png'
+        width={400}
+        height={300}
+        alt='logo'
+        priority
+      />
+      <Image
+        src='/assets/background.jpeg'
+        width={1280}
+        height={2000}
+        alt='logo'
+        priority
+        className='absolute top-0 left-0 object-cover min-h-full opacity-20'
+        style={{ filter: 'grayscale(100%) invert(200%)' }}
+      />
+    </main>
+  );
 }
